554670d429bab4ef1dca39dbb92e4013aec1552e
uliss <uliss@ULISSES-HOME-PC>
2021-09-26 09:50:20 -0300

    Adicionei 2 linhas no código

--- Hello Github/src/HelloGit.java
+++ Hello Github/src/HelloGit.java
@@ -2,7 +2,8 @@
 public class HelloGit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello Github!");
+		System.out.println("Odisseu93");
 
 	}
 
